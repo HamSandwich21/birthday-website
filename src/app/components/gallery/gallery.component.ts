936aef42ddd8fdfdf25bf0830459be70f1c907c6
HamSandwich21 <miz.el.assassino@gmail.com>
2024-10-17 23:53:41 +0100

    Revert "environment base ref"
    This reverts commit b582e6b33f8e3bd199fa2f06396725eca5d96f51.

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-gallery',
@@ -9,27 +8,11 @@ import { environment } from '../../../environments/environment';
 })
 export class GalleryComponent implements OnInit, OnDestroy {
   private audio: HTMLAudioElement;
-  public image1: string;
-  public image2: string;
-  public image3: string;
-  public image4: string;
-  public image5: string;
-  public image6: string;
-  public image7: string;
-  public image8: string;
   isMuted: boolean = false;
 
   constructor() {
     this.audio = new Audio('assets/gallery-background-music.mp3');
     this.audio.loop = false;
-    this.image1 = `${environment.baseUrl}/assets/Image 1.jpg`;
-    this.image2 = `${environment.baseUrl}/assets/Image 2.jpg`;
-    this.image3 = `${environment.baseUrl}/assets/Image 3.jpg`;
-    this.image4 = `${environment.baseUrl}/assets/Image 4.jpg`;
-    this.image5 = `${environment.baseUrl}/assets/Image 5.jpg`;
-    this.image6 = `${environment.baseUrl}/assets/Image 6.jpg`;
-    this.image7 = `${environment.baseUrl}/assets/Image 7.jpg`;
-    this.image8 = `${environment.baseUrl}/assets/Image 8.jpg`;
   }
 
   ngOnInit() {
