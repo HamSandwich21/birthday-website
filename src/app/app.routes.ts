e3af356bbe6a504d4a1cdca0d6c5071b06146b7f
HamSandwich21 <miz.el.assassino@gmail.com>
2024-10-17 22:36:34 +0100

    hashlocation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { MessagePageComponent } from './components/message-page/message-page.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
@@ -9,3 +11,10 @@ export const routes: Routes = [
   { path: 'message', component: MessagePageComponent },
   { path: 'gallery', component: GalleryComponent },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+})
+export class AppRoutingModule {}
