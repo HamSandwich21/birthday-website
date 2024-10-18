8907ab7495c8b68b30c9f2f33236cfc36a99afde
HamSandwich21 <miz.el.assassino@gmail.com>
2024-10-18 11:51:13 +0100

    fix

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
+  imports: [RouterModule],
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
 })
